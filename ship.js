081d5b4928e671b82709c3a0a56b1e92df90edbd
Joe Chambers <josephbrianchambers@gmail.com>
2020-09-17 15:00:11 -0700

    Extract ship codd to shop.js

---
+++ ship.js
@@ -0,0 +1,57 @@
+function Ship() {
+  this.pos = createVector(width/2, height/2);
+  this.r = 10;
+  this.heading = 0;
+  this.rotation = 0;
+  this.vel = createVector(0, 0);
+  this.isBoosting = false;
+
+  this.setIsBoosting = function(isBoosting) {
+    this.isBoosting = isBoosting;
+  }
+
+  this.setRotation = function(angle) {
+    this.rotation = angle;
+  }
+
+  this.boost = function() {
+    var force = p5.Vector.fromAngle(this.heading)
+    force.mult(.1)
+    this.vel.add(force)
+  }
+
+  this.turn = function() {
+    this.heading += this.rotation;
+  }
+
+  this.update = function() {
+    if (this.isBoosting) {
+      this.boost();
+    }
+
+    this.pos.add(this.vel);
+    this.vel.mult(0.99);
+  }
+
+  this.render = function() {
+    noFill();
+    stroke(255);
+    translate(this.pos.x, this.pos.y)
+    rotate(this.heading + PI / 2);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
+  }
+
+  this.edges = function() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
+}
