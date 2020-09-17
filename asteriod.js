ba843b6066e869682bcf6a63df714bff71c4d105
Joe Chambers <josephbrianchambers@gmail.com>
2020-09-17 15:20:25 -0700

    Render one asteroid

---
+++ asteriod.js
@@ -0,0 +1,14 @@
+function Asteroid() {
+  this.r = 50;
+  this.pos = createVector(random(width), random(height));
+
+  this.render = function() {
+    push();
+    stroke(255);
+    noFill();
+    translate(this.pos.x, this.pos.y);
+    ellipse(0, 0, this.r * 2);
+
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var ship;
+var asteroids = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
+  asteroids.push(new Asteroid());
 }
 
 function draw() {
@@ -11,6 +13,10 @@ function draw() {
   ship.turn()
   ship.update()
   ship.edges()
+
+  for (var i = 0; i < asteroids.length; i++) {
+    asteroids[i].render();
+  }
 }
 
 function keyPressed() {
@@ -26,62 +32,4 @@ function keyPressed() {
 function keyReleased() {
   ship.setRotation(0);
   ship.setIsBoosting(false);
-}
-
-function Ship() {
-  this.pos = createVector(width/2, height/2);
-  this.r = 10;
-  this.heading = 0;
-  this.rotation = 0;
-  this.vel = createVector(0, 0);
-  this.isBoosting = false;
-
-  this.setIsBoosting = function(isBoosting) {
-    this.isBoosting = isBoosting;
-  }
-
-  this.setRotation = function(angle) {
-    this.rotation = angle;
-  }
-
-  this.boost = function() {
-    var force = p5.Vector.fromAngle(this.heading)
-    force.mult(.1)
-    this.vel.add(force)
-  }
-
-  this.turn = function() {
-    this.heading += this.rotation;
-  }
-
-  this.update = function() {
-    if (this.isBoosting) {
-      this.boost();
-    }
-
-    this.pos.add(this.vel);
-    this.vel.mult(0.99);
-  }
-
-  this.render = function() {
-    noFill();
-    stroke(255);
-    translate(this.pos.x, this.pos.y)
-    rotate(this.heading + PI / 2);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-  }
-
-  this.edges = function() {
-    if (this.pos.x > width + this.r) {
-      this.pos.x = -this.r;
-    } else if (this.pos.x < -this.r) {
-      this.pos.x = width + this.r;
-    }
-
-    if (this.pos.y > height + this.r) {
-      this.pos.y = -this.r;
-    } else if (this.pos.y < -this.r) {
-      this.pos.y = height + this.r;
-    }
-  }
 }
