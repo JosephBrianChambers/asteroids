531d7e80dc81d89c91eb7b5685b90611ac1755d1
Joe Chambers <josephbrianchambers@gmail.com>
2020-09-17 15:40:52 -0700

    Render stylized asteroids

--- sketch.js
+++ sketch.js
@@ -4,7 +4,10 @@ var asteroids = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
-  asteroids.push(new Asteroid());
+
+  for (var i = 0; i < 5; i++) {
+    asteroids.push(new Asteroid());
+  }
 }
 
 function draw() {
@@ -16,6 +19,8 @@ function draw() {
 
   for (var i = 0; i < asteroids.length; i++) {
     asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
   }
 }
 
